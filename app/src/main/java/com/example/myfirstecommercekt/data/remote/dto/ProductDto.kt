b4a81ca5c72299af6040df7fceaa35a95597751f
Macarena <macarena.zalazar@gmail.com>
2025-07-07 21:12:19 -0300

    update productdto

---
+++ app/src/main/java/com/example/myfirstecommercekt/data/remote/dto/ProductDto.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstecommercekt.data.remote.dto
+
+import com.example.myfirstecommercekt.data.local.entity.*
+import com.example.myfirstecommercekt.utils.data.*
+import com.google.gson.annotations.*
+
+data class ProductDto(
+    @SerializedName("_id")
+    val id: String,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val category: String,
+    val imageUrl: String
+) {
+    fun toDomain() = Product(id, name, description, price, category, imageUrl)
+    fun toEntity() = ProductEntity(
+        name = name,
+        description = description,
+        price = price,
+        category = category,
+        imageUrl = imageUrl
+    )
+}
