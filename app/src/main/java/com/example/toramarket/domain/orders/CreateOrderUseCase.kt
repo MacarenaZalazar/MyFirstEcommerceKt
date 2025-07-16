d681c358d3a9d8e36812ef8498b656a8d95375db
Macarena <macarena.zalazar@gmail.com>
2025-07-16 00:35:01 -0300

    order use case

--- app/src/main/java/com/example/toramarket/domain/orders/CreateOrderUseCase.kt
+++ app/src/main/java/com/example/toramarket/domain/orders/CreateOrderUseCase.kt
@@ -1,4 +1,13 @@
 package com.example.toramarket.domain.orders
 
-class CreateOrderUseCase {
+import com.example.toramarket.data.remote.dto.*
+import com.example.toramarket.data.repository.interfaces.*
+import com.example.toramarket.utils.data.*
+import javax.inject.*
+
+class CreateOrderUseCase @Inject constructor(private val repository: OrderRepository) {
+    suspend operator fun invoke(userId: String, items: List<OrderItem>): OrderDto {
+        val order = NewOrderDto(userId = userId, items = items.map { it.toDto() })
+        return repository.createOrder(order)
+    }
 }
